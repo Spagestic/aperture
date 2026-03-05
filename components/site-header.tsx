a0d12a63b689e9a073f2c4cc5581dafcd3921f5c
spagestic <steelmax666@gmail.com>
2026-03-05 16:02:59 +0800

    dummy Searchbar

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,30 +1,28 @@
-import { Button } from "@/components/ui/button"
-import { Separator } from "@/components/ui/separator"
-import { SidebarTrigger } from "@/components/ui/sidebar"
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { IconShare3 } from "@tabler/icons-react";
+import { Searchbar } from "./search-bar";
 
 export function SiteHeader() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1" />
-        <Separator
-          orientation="vertical"
-          className="mx-2 data-[orientation=vertical]:h-4"
-        />
-        <h1 className="text-base font-medium">Documents</h1>
-        <div className="ml-auto flex items-center gap-2">
-          <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
-            <a
-              href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
-              rel="noopener noreferrer"
-              target="_blank"
-              className="dark:text-foreground"
-            >
-              GitHub
-            </a>
+      <div className="flex w-full items-center justify-between gap-1 px-4 lg:gap-2 lg:px-6">
+        <div className="flex items-center gap-1">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mx-2 data-[orientation=vertical]:h-4"
+          />
+        </div>
+        <Searchbar />
+        <div className=" flex items-center gap-2">
+          <Button variant="ghost" size="sm">
+            <IconShare3 />
+            <span className="hidden lg:inline">Share</span>
           </Button>
         </div>
       </div>
     </header>
-  )
+  );
 }
