8aa1e928cf14188191a038461bd78ad479e32d10
spagestic <steelmax666@gmail.com>
2026-03-07 14:41:59 +0800

    sidebar width reduced

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <SidebarProvider
       style={
         {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--sidebar-width": "calc(var(--spacing) * 48)",
           "--header-height": "calc(var(--spacing) * 12)",
         } as React.CSSProperties
       }
