cc904478c21e5beba9b13714b69243cccfdbd02d
spagestic <steelmax666@gmail.com>
2026-04-12 10:11:12 +0800

    refactor: simplify SiteHeader component by removing unused imports and theme handling

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,31 +1,15 @@
 "use client";
 
 import * as React from "react";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
-import { useTheme } from "next-themes";
-import {
-  IconShare3,
-  IconBell,
-  IconMoonStars,
-  IconSunHigh,
-} from "@tabler/icons-react";
+
 import { Searchbar } from "./search-bar";
 import { UserAvatar } from "./user-avatar";
 
 export function SiteHeader() {
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = React.useState(false);
-
-  React.useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  const isDark = theme === "dark";
-
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+    <header className="sticky top-0 z-50 flex h-(--header-height) shrink-0 items-center gap-2 border-b bg-background  transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center justify-between gap-1 px-4 lg:gap-2 lg:px-6">
         <div className="flex items-center gap-1">
           {" "}
